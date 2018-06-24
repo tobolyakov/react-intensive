bb3544f6cd4cdfd88e6b3e9873aa05387a26458d
Igor <igor.tobolyakov@yahoo.com>
2018-06-24 15:13:26 +0300

    add components Post add css

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 
 // Instuments
 import avatar from '../../theme/assets/homer.png';
+import Styles from './style.m.css';
 
 export default class Post extends Component{
     render () {
@@ -12,12 +13,11 @@ export default class Post extends Component{
             width: 'calc(100/4*1%)'
         };
         return (
-            <section>
-                <div>
-                    <img src={ avatar } style={ style } />
-                    <a>Igor</a>
-                    <time>{ time }</time>
-                </div>
+            <section className={Styles.post}>
+                <span className={Styles.cross}></span>
+                <img src={ avatar }/>
+                <a>Igor</a>
+                <time>{ time }</time>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </section>
         )
