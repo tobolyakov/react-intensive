36dc1235614e3aa439d99aa8b65c1b122b158145
Igor <igor.tobolyakov@yahoo.com>
2018-06-24 14:56:48 +0300

    add components Composer add css

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 
 // Instuments
 import avatar from '../../theme/assets/homer.png';
+import Styles from './style.m.css';
 
 export default class Composer extends Component{
     render () {
         return (
-            <section>
+            <section className={Styles.composer}>
                 <img src={ avatar } />
                 <form>
                     <textarea placeholder={ `Wat is in your mind, Igor` } />
