72d7f767ba3989a19c3554a74511b29265aed146
Igor <igor.tobolyakov@yahoo.com>
2018-06-25 14:29:50 +0300

    add hoc

--- source/components/HOC/withProfile.js
+++ source/components/HOC/withProfile.js
@@ -1,11 +1,24 @@
 //Core
-import React, { createContext } from 'react';
+import React, { createContext, Component } from 'react';
 
 // Instruments
 
 //
 
 const { Provider, Consumer } = createContext();
+
+const withProfile = (Enhanceable) =>
+    class  WithProfile extends Component {
+        render () {
+            return (
+                <Consumer>
+                    {
+                        (context) => <Enhanceable { ...context } { ...this.props } />
+                    }
+                </Consumer>
+            );
+        }
+    };
 
 
 // Components
@@ -14,4 +27,4 @@ const { Provider, Consumer } = createContext();
 export {
     Provider,
     Consumer,
-}
+};
